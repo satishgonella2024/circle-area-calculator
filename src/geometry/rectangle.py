2097f34a808317ff09755212628bdd05f18133e9
Subrahmanya Gonella <164250924+satishgonella2024@users.noreply.github.com>
2025-07-17 19:12:01 +0100

    Initial commit: QLCapsule
    Generated by Quantum Layer Platform

---
+++ src/geometry/rectangle.py
@@ -0,0 +1,13 @@
+def calculate_rectangle_area(width, height):
+    """
+    Calculates the area of a rectangle.
+    
+    Args:
+        width (float): The width of the rectangle.
+        height (float): The height of the rectangle.
+    
+    Returns:
+        float: The area of the rectangle.
+    """
+    area = width * height
+    return area
